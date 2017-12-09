a533cf8fcfb212b6d307ad57316eb12460d56755
Marek Juszczak <juszczak.marek@gmail.com>
2017-12-09 13:13:23 +0100

    Drugie zadanie z synchronizacją wątków

--- src/mju/watki/SynchronizacjaMonitora.java
+++ src/mju/watki/SynchronizacjaMonitora.java
@@ -1,27 +1,39 @@
 package mju.watki;
 
-//Stworz klase SynchronizacjaMonitora.
-//W klasie utwórz pole statyczne typu String i nazwij je monitor
-//W metodzie main tej klasy umieść pętlę for która wykona dziesiec iteracji.
-//Dla kazdej iteracji utwórz nowy wątek.
-//W metodzie run zsynchronizuj się na monitorze, nastepnie uspij watek na dziesiec sekund, na koniec wypisz na ekran:
-//a) identyfikator wątku
-//b) numer iteracji w której wątek został utworzony
-//Wystartuj utworzony wątek
-//Jak zachowa się program gdy w metodzie run zakomentujesz sekcję synchronizowana?
+//Na podstawie zadanie 3 (branch github: Ex03_Synchronizacja01)
+//Utwórz w klasie SynchronizacjaMonitora pole statyczne typu int o nazwie wartoscWspoldzielona
+//Zmodyfikuj metode run w ten sposob ze zakomentujesz blok synchronized.
+//Kolejno, wypiszesz na ekran id watku oraz wartosc pola wartoscWspoldzielona.
+//Nastepnie uspisz watek na 3 sekundy.
+//Wygenerujesz losowa liczbe z przedzialu od 0 do 100
+//Wypiszez na ekran id watku oraz wygenerowana liczbe.
+//Nastepnie zwiekszysz wartoscWspoldzielona o wygenerowana liczbe.
+//Wypiszesz na ekran id watku oraz wartosc zmiennej wspoldzielonej.
+//Czy wyniki zmienia sie jezeli przywrocisz synchronizacje?
+
+import java.util.Random;
 
 public class SynchronizacjaMonitora {
 
     private static String monitor = "";
+    private static int wartoscWspoldzielona = 0;
 
     public static void main(String[] args) {
+        Random random = new Random();
+
         for (int i = 0; i < 10; i++) {
-            final int iterator = i;
+//            final int iterator = i;
             new Thread(() -> {
                 synchronized (monitor) {
                     try {
-                        Thread.sleep(1000);
-                        System.out.println("i = " + iterator + " ID = " + Thread.currentThread().getId());
+                        System.out.println("(ID) = " + Thread.currentThread().getId() + ", wartoscWspoldzielona = " + wartoscWspoldzielona);
+                        Thread.sleep(3000);
+                        int randomInt = random.nextInt(100);
+                        System.out.println("(ID) = " + Thread.currentThread().getId() + ", wartoscLosowa = " + randomInt);
+                        System.out.print("(ID) = " + Thread.currentThread().getId()+ ", " + wartoscWspoldzielona + " + " + randomInt + " = ");
+                        wartoscWspoldzielona+=randomInt;
+                        System.out.print(wartoscWspoldzielona);
+                        System.out.println("\n");
                     } catch (InterruptedException e) {
                         e.printStackTrace();
                     }
